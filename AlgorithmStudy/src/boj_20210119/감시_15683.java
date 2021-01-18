f86397ca357e3d5b6810eb522983d83a5c176b6b
Sn-Hn <tlsgks8668@naver.com>
2021-01-19 00:05:55 +0900

    Study

---
+++ AlgorithmStudy/src/boj_20210119/감시_15683.java
@@ -0,0 +1,294 @@
+package boj_20210119;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.StringTokenizer;
+
+/*
+
+출처 : https://www.acmicpc.net/problem/15683
+
+감시 분류
+시간 제한	메모리 제한	제출	정답	맞은 사람	정답 비율
+1 초	512 MB	20631	9189	5343	40.943%
+문제
+스타트링크의 사무실은 1×1크기의 정사각형으로 나누어져 있는 N×M 크기의 직사각형으로 나타낼 수 있다. 
+사무실에는 총 K개의 CCTV가 설치되어져 있는데, CCTV는 5가지 종류가 있다. 각 CCTV가 감시할 수 있는 방법은 다음과 같다.
+
+(백준 15683_감시 문제에 그림보기)
+
+(그림)
+1번	2번	3번	4번	5번
+1번 CCTV는 한 쪽 방향만 감시할 수 있다. 
+2번과 3번은 두 방향을 감시할 수 있는데, 2번은 감시하는 방향이 서로 반대방향이어야 하고, 3번은 직각 방향이어야 한다. 4번은 세 방향, 5번은 네 방향을 감시할 수 있다.
+
+CCTV는 감시할 수 있는 방향에 있는 칸 전체를 감시할 수 있다. 사무실에는 벽이 있는데, CCTV는 벽을 통과할 수 없다. CCTV가 감시할 수 없는 영역은 사각지대라고 한다.
+
+CCTV는 회전시킬 수 있는데, 회전은 항상 90도 방향으로 해야 하며, 감시하려고 하는 방향이 가로 또는 세로 방향이어야 한다.
+
+0 0 0 0 0 0
+0 0 0 0 0 0
+0 0 1 0 6 0
+0 0 0 0 0 0
+지도에서 0은 빈 칸, 6은 벽, 1~5는 CCTV의 번호이다. 위의 예시에서 1번의 방향에 따라 감시할 수 있는 영역을 '#'로 나타내면 아래와 같다.
+
+0 0 0 0 0 0
+0 0 0 0 0 0
+0 0 1 # 6 0
+0 0 0 0 0 0
+→
+
+0 0 0 0 0 0
+0 0 0 0 0 0
+# # 1 0 6 0
+0 0 0 0 0 0
+←
+
+0 0 # 0 0 0
+0 0 # 0 0 0
+0 0 1 0 6 0
+0 0 0 0 0 0
+↑
+
+0 0 0 0 0 0
+0 0 0 0 0 0
+0 0 1 0 6 0
+0 0 # 0 0 0
+↓
+CCTV는 벽을 통과할 수 없기 때문에, 1번이 → 방향을 감시하고 있을 때는 6의 오른쪽에 있는 벽을 감시할 수 없다.
+
+0 0 0 0 0 0
+0 2 0 0 0 0
+0 0 0 0 6 0
+0 6 0 0 2 0
+0 0 0 0 0 0
+0 0 0 0 0 5
+위의 예시에서 감시할 수 있는 방향을 알아보면 아래와 같다.
+
+0 0 0 0 0 #
+# 2 # # # #
+0 0 0 0 6 #
+0 6 # # 2 #
+0 0 0 0 0 #
+# # # # # 5
+왼쪽 상단 2: ↔, 오른쪽 하단 2: ↔
+
+0 0 0 0 0 #
+# 2 # # # #
+0 0 0 0 6 #
+0 6 0 0 2 #
+0 0 0 0 # #
+# # # # # 5
+왼쪽 상단 2: ↔, 오른쪽 하단 2: ↕
+
+0 # 0 0 0 #
+0 2 0 0 0 #
+0 # 0 0 6 #
+0 6 # # 2 #
+0 0 0 0 0 #
+# # # # # 5
+왼쪽 상단 2: ↕, 오른쪽 하단 2: ↔
+
+0 # 0 0 0 #
+0 2 0 0 0 #
+0 # 0 0 6 #
+0 6 0 0 2 #
+0 0 0 0 # #
+# # # # # 5
+왼쪽 상단 2: ↕, 오른쪽 하단 2: ↕
+
+CCTV는 CCTV를 통과할 수 있다. 아래 예시를 보자.
+
+0 0 2 0 3
+0 6 0 0 0
+0 0 6 6 0
+0 0 0 0 0
+위와 같은 경우에 2의 방향이 ↕ 3의 방향이 ←와 ↓인 경우 감시받는 영역은 다음과 같다.
+
+# # 2 # 3
+0 6 # 0 #
+0 0 6 6 #
+0 0 0 0 #
+사무실의 크기와 상태, 그리고 CCTV의 정보가 주어졌을 때, CCTV의 방향을 적절히 정해서, 사각 지대의 최소 크기를 구하는 프로그램을 작성하시오.
+
+입력
+첫째 줄에 사무실의 세로 크기 N과 가로 크기 M이 주어진다. (1 ≤ N, M ≤ 8)
+
+둘째 줄부터 N개의 줄에는 사무실 각 칸의 정보가 주어진다. 0은 빈 칸, 6은 벽, 1~5는 CCTV를 나타내고, 문제에서 설명한 CCTV의 종류이다. 
+
+CCTV의 최대 개수는 8개를 넘지 않는다.
+
+출력
+첫째 줄에 사각 지대의 최소 크기를 출력한다.
+
+예제 입력 1 
+4 6
+0 0 0 0 0 0
+0 0 0 0 0 0
+0 0 1 0 6 0
+0 0 0 0 0 0
+예제 출력 1 
+20
+예제 입력 2 
+6 6
+0 0 0 0 0 0
+0 2 0 0 0 0
+0 0 0 0 6 0
+0 6 0 0 2 0
+0 0 0 0 0 0
+0 0 0 0 0 5
+예제 출력 2 
+15
+예제 입력 3 
+6 6
+1 0 0 0 0 0
+0 1 0 0 0 0
+0 0 1 0 0 0
+0 0 0 1 0 0
+0 0 0 0 1 0
+0 0 0 0 0 1
+예제 출력 3 
+6
+예제 입력 4 
+6 6
+1 0 0 0 0 0
+0 1 0 0 0 0
+0 0 1 5 0 0
+0 0 5 1 0 0
+0 0 0 0 1 0
+0 0 0 0 0 1
+예제 출력 4 
+2
+예제 입력 5 
+1 7
+0 1 2 3 4 5 6
+예제 출력 5 
+0
+예제 입력 6 
+3 7
+4 0 0 0 0 0 0
+0 0 0 2 0 0 0
+0 0 0 0 0 0 4
+예제 출력 6 
+0
+
+*/
+
+// 감시
+public class 감시_15683 {
+	// N : 세로, M : 가로
+	private static int N, M;
+	private static int map[][];
+	private static int min = 1000000000;
+	private static List<Point> cctv = new ArrayList<>();
+
+	// 					오른(R), 아래(B), 왼(L), 위(U)
+	private static int[] dx = { 0, 1, 0, -1 };
+	private static int[] dy = { 1, 0, -1, 0 };
+
+	private static int[][] direction = { 
+			{}, 			// cctvNum
+			{ 0 }, 			// 1
+			{ 0, 2 },		// 2
+			{ 0, 1 }, 		// 3
+			{ 0, 1, 2 }, 	// 4
+			{ 0, 1, 2, 3 } 	// 5
+	};
+
+	static class Point {
+		int x, y, cctvNum;
+
+		public Point(int x, int y, int cctvNum) {
+			this.x = x;
+			this.y = y;
+			this.cctvNum = cctvNum;
+		}
+	}
+
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st = new StringTokenizer(br.readLine());
+
+		N = Integer.parseInt(st.nextToken());
+		M = Integer.parseInt(st.nextToken());
+		map = new int[N][M];
+
+		for (int i = 0; i < N; i++) {
+			st = new StringTokenizer(br.readLine());
+			for (int j = 0; j < M; j++) {
+				map[i][j] = Integer.parseInt(st.nextToken());
+				if (map[i][j] != 0 && map[i][j] != 6) {
+					cctv.add(new Point(i, j, map[i][j]));
+				}
+			}
+		}
+
+		compareSurveillance(0, map);
+
+		System.out.println(min);
+
+		br.close();
+	}
+
+	private static void compareSurveillance(int idx, int arr[][]) {
+
+		if (idx == cctv.size()) {
+			min = Math.min(min, countMin(arr));
+			return;
+		}
+
+		Point p = cctv.get(idx);
+
+		for (int k = 0; k < 4; k++) {
+			int copyMap[][] = copyMap(arr);
+			int index = p.cctvNum;
+			// 아래 for문은 1 ~ 5번까지의 cctv를 방향을 고려하지 않고 오른쪽 기준으로 각각의 cctv가 볼 수 있는 구역을 구함
+			for (int i = 0; i < direction[index].length; i++) {
+				int dir = (direction[index][i] + k) % 4;
+				int X = p.x;
+				int Y = p.y;
+				while (true) {
+					X += dx[dir];
+					Y += dy[dir];
+
+					if (X >= 0 && X < N && Y >= 0 && Y < M && map[X][Y] != 6) {
+						// 9는 감시할 수 있는 영역
+						copyMap[X][Y] = 9;
+					} else {
+						break;
+					}
+
+				}
+			}
+			compareSurveillance(idx + 1, copyMap);
+
+		}
+
+	}
+
+	private static int[][] copyMap(int arr[][]) {
+		int copyMap[][] = new int[N][M];
+		for (int i = 0; i < N; i++) {
+			for (int j = 0; j < M; j++) {
+				copyMap[i][j] = arr[i][j];
+			}
+		}
+
+		return copyMap;
+	}
+
+	private static int countMin(int copyMap[][]) {
+		int count = 0;
+		for (int i = 0; i < N; i++) {
+			for (int j = 0; j < M; j++) {
+				if (copyMap[i][j] == 0) {
+					count++;
+				}
+			}
+		}
+		return count;
+	}
+}

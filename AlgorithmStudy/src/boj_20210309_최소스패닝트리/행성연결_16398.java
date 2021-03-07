2b4231edeaa6c7368430f28a9fe697ec399abe39
ShinHan <tlsgks8668@naver.com>
2021-03-08 00:40:36 +0900

    최소 스패닝 트리 (크루스칼 알고리즘)

---
+++ AlgorithmStudy/src/boj_20210309_최소스패닝트리/행성연결_16398.java
@@ -0,0 +1,136 @@
+package boj_20210309_최소스패닝트리;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.PriorityQueue;
+import java.util.StringTokenizer;
+
+/*
+
+행성 연결 출처분류
+시간 제한	메모리 제한	제출	정답	맞은 사람	정답 비율
+1 초	256 MB	1859	772	503	37.877%
+문제
+홍익 제국의 중심은 행성 T이다. 제국의 황제 윤석이는 행성 T에서 제국을 효과적으로 통치하기 위해서, N개의 행성 간에 플로우를 설치하려고 한다.
+
+두 행성 간에 플로우를 설치하면 제국의 함선과 무역선들은 한 행성에서 다른 행성으로 무시할 수 있을 만큼 짧은 시간만에 이동할 수 있다. 
+하지만, 치안을 유지하기 위해서 플로우 내에 제국군을 주둔시켜야 한다.
+
+모든 행성 간에 플로우를 설치하고 플로우 내에 제국군을 주둔하면, 제국의 제정이 악화되기 때문에 황제 윤석이는 제국의 모든 행성을 연결하면서 플로우 관리 비용을 최소한으로 하려 한다.
+
+N개의 행성은 정수 1,…,N으로 표시하고, 행성 i와 행성 j사이의 플로우 관리비용은 Cij이며, i = j인 경우 항상 0이다.
+
+제국의 참모인 당신은 제국의 황제 윤석이를 도와 제국 내 모든 행성을 연결하고, 그 유지비용을 최소화하자.  
+이때 플로우의 설치비용은 무시하기로 한다.
+
+입력
+입력으로 첫 줄에 행성의 수 N (1 ≤ N ≤ 1000)이 주어진다.
+
+두 번째 줄부터 N+1줄까지 각 행성간의 플로우 관리 비용이 N x N 행렬 (Cij),  (1 ≤ i, j ≤ N, 1 ≤ Cij ≤ 100,000,000, Cij = Cji) 로 주어진다.
+
+출력
+모든 행성을 연결했을 때, 최소 플로우의 관리비용을 출력한다.
+
+예제 입력 1 
+3
+0 2 3
+2 0 1
+3 1 0
+예제 출력 1 
+3
+예제 입력 2 
+5
+0 6 8 1 3
+6 0 5 7 3
+8 5 0 9 4
+1 7 9 0 6
+3 3 4 6 0
+예제 출력 2 
+11
+
+*/
+
+public class 행성연결_16398 {
+	private static int N;
+	private static int map[][];
+	private static int parent[];
+	
+	private static class Node implements Comparable<Node>{
+		int x, y, cost;
+		public Node(int x, int y, int cost) {
+			this.x = x;
+			this.y = y;
+			this.cost = cost;
+		}
+		
+		@Override
+		public int compareTo(Node o) {
+			// TODO Auto-generated method stub
+			return cost - o.cost;
+		}
+	}
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		N = Integer.parseInt(br.readLine());
+		map = new int[N+1][N+1];
+		parent = new int[N+1];
+		
+		PriorityQueue<Node> q = new PriorityQueue<Node>();
+		for(int i = 1; i <= N; i++) {
+			parent[i] = i;
+		}
+		
+		for(int i = 1; i <= N; i++) {
+			StringTokenizer st = new StringTokenizer(br.readLine());
+			for(int j = 1; j <= N; j++) {
+				map[i][j] = Integer.parseInt(st.nextToken());
+			}
+		}
+		
+		for(int i = 1; i < N; i++) {
+			for(int j = i+1; j <= N; j++) {
+				q.add(new Node(i, j, map[i][j]));
+			}
+		}
+		solve(q);
+		
+		br.close();
+	}
+	
+	private static void solve(PriorityQueue<Node> q) {
+		long cost = 0;
+		int count = 0;
+		while(!q.isEmpty() && count < N-1) {
+			Node node = q.poll();
+			
+			if(!findCycle(node.x, node.y)) {
+				union(node.x, node.y);
+				cost += node.cost;
+				count++;
+			}
+		}
+		System.out.println(cost);
+	}
+	
+	private static int find(int x) { 
+		if(parent[x] == x) return x;
+		return parent[x] = find(parent[x]);
+	}
+	
+	private static void union(int a, int b) {
+		a = find(a);
+		b = find(b);
+		
+		if(a < b) parent[b] = a;
+		else parent[a] = b;
+	}
+	
+	private static boolean findCycle(int a, int b) {
+		a = find(a);
+		b = find(b);
+		
+		if(a == b) return true;
+		return false;
+	}
+}
